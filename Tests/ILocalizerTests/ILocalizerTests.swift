e9f6af481d97b94a3cc7b9cb5cb655adf9b4365d
Jamal alayq <gamalal3yk@gmail.com>
2019-09-30 11:02:24 +0200

    Fix Source path

--- Tests/ILocalizerTests/ILocalizerTests.swift
+++ Tests/ILocalizerTests/ILocalizerTests.swift
@@ -2,14 +2,8 @@ import XCTest
 @testable import ILocalizer
 
 final class ILocalizerTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(ILocalizer().text, "Hello, World!")
-    }
 
-    static var allTests = [
+    /*static var allTests = [
         ("testExample", testExample),
-    ]
+    ]*/
 }
